845554e608c33d731ffeae9e63322cae99543d31
unknown <ilianjesus14@outlook.com>
2024-08-07 22:19:49 -0600

    First commit, configuration venv and first model

---
+++ inscripciones/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+    fecha_nacimiento = models.DateField()
+    direccion = models.TextField()
+    email = models.EmailField()
+    telefono = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.apellidos + ' ' + self.nombre
+    
