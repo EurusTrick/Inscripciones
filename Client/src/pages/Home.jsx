1e9b6c40a0e284de087f3d19336b45788c460510
unknown <ilianjesus14@outlook.com>
2024-08-15 15:55:03 -0600

    personalizacion al home

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -6,7 +6,18 @@ export function Home() {
         <div>
             <h1>Hola Ilian</h1>
             <p>¡Es un gusto verte de nuevo!</p>
-
+            <p>
+            EasyEnroll es una aplicación diseñada para <br/>
+            facilitar la gestión de inscripciones, pagos, <br/>
+            alumnos y tutores.
+            </p>
+            <p>
+            En el menu inferior puedes: <br/>
+            - Gestionar alumnos <br/>
+            - Gestionar tutores <br/>
+            - Gestionar pagos <br/>
+            - Gestionar inscripciones
+            </p>
 
             <div>
             <Link to="/alumnos"><h1>Alumnos</h1></Link>
@@ -14,6 +25,8 @@ export function Home() {
             <Link to="/pagos"><h1>Pagos</h1></Link>
             <Link to="/inscripciones"><h1>Inscripciones</h1></Link>
             </div>
+
+            
             
         </div>
     );
