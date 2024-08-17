cc711c60459eb1dc1ae3b2cb2f12351bd379b619
unknown <ilianjesus14@outlook.com>
2024-08-17 01:17:26 -0600

    Agregamos logo y personalizamos detalles en el home

--- Client/src/components/Navigation.jsx
+++ Client/src/components/Navigation.jsx
@@ -1,23 +1,30 @@
 import { Link } from "react-router-dom";
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserGraduate, faUser, faHome, faMoneyBill, faClipboardList } from "@fortawesome/free-solid-svg-icons";
 
 export function Navigation() {
     return (
         <div className="fixed bottom-0 left-0 w-full bg-gray-800">
             <div className="flex justify-around p-4">
-                <Link to="/alumnos" className="text-white">
+                <Link to="/alumnos" className="text-white flex flex-col items-center">
+                    <FontAwesomeIcon icon={faUserGraduate} className="text-white mb-1" />
                     Alumnos
                 </Link>
-                <Link to="/tutores" className="text-white">
+                <Link to="/tutores" className="text-white flex flex-col items-center">
+                    <FontAwesomeIcon icon={faUser} className="text-white mb-1" />
                     Padres
                 </Link>
-                <Link to="/" className="text-white">
+                <Link to="/" className="text-white flex flex-col items-center">
+                    <FontAwesomeIcon icon={faHome} className="text-white mb-1" />
                     Home
                 </Link>
-                <Link to="/pagos" className="text-white">
+                <Link to="/pagos" className="text-white flex flex-col items-center">
+                    <FontAwesomeIcon icon={faMoneyBill} className="text-white mb-1" />
                     Pagos
                 </Link>
-                <Link to="/inscripciones" className="text-white">
+                <Link to="/inscripciones" className="text-white flex flex-col items-center">
+                    <FontAwesomeIcon icon={faClipboardList} className="text-white mb-1" />
                     Inscripciones
                 </Link>
             </div>
--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,18 +1,20 @@
 /*import { Link } from "react-router-dom";*/
 import React from "react";
+import Logo from "../assets/Logo.png";
 
 export function Home() {
     return (
         <div className="flex flex-col h-screen justify-between">
-            <div className="text-center p-4">
-                <h1>Hola Ilian</h1>
-                <p>¡Es un gusto verte de nuevo!</p>
-                <p>
+            <div className="text-left p-4 mx-auto max-w-lg" >
+                <h1 className="text-3xl font-bold text-center">Hola Ilian</h1>
+                <p className="text-center">¡Es un gusto verte de nuevo!</p>
+                
+                <img src={Logo} alt="Logo" className="mx-auto w-[300px] h-[250px] "/>
+                <p className="text-left p-3 mx-auto max-w-lg ">
                     EasyEnroll es una aplicación diseñada para <br/>
                     facilitar la gestión de inscripciones, pagos, <br/>
-                    alumnos y tutores.
-                </p>
-                <p>
+                    alumnos y tutores. <br/>
+                    <br/>
                     En el menú inferior puedes: <br/>
                     - Gestionar alumnos <br/>
                     - Gestionar tutores <br/>
